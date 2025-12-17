21d5adb94b4f23d9ee62a248d76d59d3715aafd8
RakkeshFireSky <rakkeshkumar@firesky.cc>
2025-12-17 21:12:11 +0530

    Add Spinner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-import Image from "next/image";
-import { redirect } from "next/navigation";
+'use client'
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import CircularProgress from '@mui/material/CircularProgress';
+import { Box } from "@mui/material";
+
 export default function Home() {
-  redirect('/users')
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (loading) {
+      router.push('/users')
+    }
+  }, [router, loading])
+
+  return (
+    <Box style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      {loading && <CircularProgress />}
+    </Box>
+
+  )
 }
